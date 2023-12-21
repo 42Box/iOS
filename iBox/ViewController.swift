348518f44ac70e26ac8096a359839063f37449d7
chan <chanhihi55@gmail.com>
2023-12-21 20:49:11 +0900

    feat: init 🎉

---
+++ iBox/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  iBox
+//
+//  Created by 김찬희 on 2023/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
