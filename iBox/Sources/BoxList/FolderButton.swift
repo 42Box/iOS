6ee34c49d6effe2535a687a82cc538713587c4e5
JH713 <strider13@snu.ac.kr>
2024-01-04 17:38:20 +0900

    feat: 폴더 버튼 디자인

---
+++ iBox/Sources/BoxList/FolderButton.swift
@@ -0,0 +1,12 @@
+//
+//  FolderButton.swift
+//  iBox
+//
+//  Created by 이지현 on 1/4/24.
+//
+
+import UIKit
+
+class FolderButton: UButton {
+
+}
---
+++ iBox/Sources/BoxList/FolderHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  FolderHeaderView.swift
+//  iBox
+//
+//  Created by 이지현 on 1/4/24.
+//
+
+import UIKit
+
+class FolderHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
