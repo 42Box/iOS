bbc9a631292815fd6631195da26fe3f80f0bc308
chanhihi <chanhihi55@gmail.com>
2024-03-02 16:07:23 +0900

    fix: version Info json

--- iBox/Sources/Model/VersionInfo.swift
+++ iBox/Sources/Model/VersionInfo.swift
@@ -5,8 +5,23 @@
 //  Created by Chan on 2/29/24.
 //
 
+// MARK: - VersionInfo
 struct VersionInfo: Codable {
-    let latestVersion: String
-    let minRequiredVersion: String
-    let updateUrl: String
+    let version: [Version]
+    let url: URLClass
+}
+
+// MARK: - URLClass
+struct URLClass: Codable {
+    let updateURL: String?
+
+    enum CodingKeys: String, CodingKey {
+        case updateURL = "updateUrl"
+    }
+}
+
+// MARK: - Version
+struct Version: Codable {
+    let id: Int
+    let latestVersion, minRequiredVersion: String
 }
