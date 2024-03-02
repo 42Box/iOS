1e336e64fea7d156f6fc5b8667ae7d689136d61b
chanhihi <chanhihi55@gmail.com>
2024-03-02 16:08:34 +0900

    feat: add versioning handler in appdelegate

--- iBox/Sources/AppDelegate.swift
+++ iBox/Sources/AppDelegate.swift
@@ -10,11 +10,11 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    let versioningHandler: VersioningHandler = VersioningHandler()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        versioningHandler.checkAppVersion()
         return true
     }
 
