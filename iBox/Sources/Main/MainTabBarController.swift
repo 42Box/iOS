7f48608e68ec40f088bc688845f7ccd25ff44fb7
noeyiz <lamoureclatant@gmail.com>
2024-03-14 05:28:55 +0900

    feat: 설정 탭 선택 시 탭바 아이템 이미지 움직이게 변경

--- iBox/Sources/Main/MainTabBarController.swift
+++ iBox/Sources/Main/MainTabBarController.swift
@@ -10,6 +10,8 @@ import UIKit
 class MainTabBarController: UITabBarController {
     
     var previousTabIndex = 0
+    var imageChangeTimer: Timer?
+    var currentImageIndex = 0
     
     // MARK: - Life Cycle
     
@@ -28,7 +30,7 @@ class MainTabBarController: UITabBarController {
         viewControllers = [
             setupViewController(viewController: BoxListViewController(), image: UIImage(systemName: "square.grid.2x2.fill")),
             setupViewController(viewController: FavoriteViewController(), image: UIImage(systemName: "heart.fill")),
-            setupViewController(viewController: SettingsViewController(), image: UIImage(systemName: "person.fill"))
+            setupViewController(viewController: SettingsViewController(), image: UIImage(named: "sitting_fox0"))
         ]
         tabBar.tintColor = .box
         tabBar.backgroundColor = .backgroundColor
@@ -46,14 +48,43 @@ class MainTabBarController: UITabBarController {
         appearance.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.clear], for: .normal)
         appearance.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.clear], for: .selected)
     }
-
+    
 }
 
 extension MainTabBarController: UITabBarControllerDelegate {
+    
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
         if tabBarController.selectedIndex == 1 && previousTabIndex == 1 {
             WebViewPreloader.shared.resetFavoriteView()
         }
         previousTabIndex = tabBarController.selectedIndex
+        
+        if tabBarController.selectedIndex == 2 {
+            startImageRotation()
+        } else {
+            stopImageRotation()
+        }
     }
+    
+    private func startImageRotation() {
+        stopImageRotation() // 현재 진행 중인 타이머가 있다면 중지
+        imageChangeTimer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateTabBarImage), userInfo: nil, repeats: true)
+    }
+    
+    private func stopImageRotation() {
+        imageChangeTimer?.invalidate()
+        imageChangeTimer = nil
+    }
+    
+    @objc private func updateTabBarImage() {
+        if let viewControllers = viewControllers, viewControllers.count > 2 {
+            let settingsViewController = viewControllers[2]
+            let imageName = "sitting_fox\(currentImageIndex)"
+            settingsViewController.tabBarItem.image = UIImage(named: imageName)
+            
+            currentImageIndex += 1
+            if currentImageIndex > 3 { currentImageIndex = 0 }
+        }
+    }
+    
 }
