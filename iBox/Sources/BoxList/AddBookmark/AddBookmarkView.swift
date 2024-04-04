007660bf9822439b4a54bad1f16357a80b932da8
jonnwon <bssong35@gmail.com>
2024-04-05 00:47:20 +0900

    refactor  : urlTextView내 기존 키보드 타입 삭제

--- iBox/Sources/BoxList/AddBookmark/AddBookmarkView.swift
+++ iBox/Sources/BoxList/AddBookmark/AddBookmarkView.swift
@@ -62,9 +62,8 @@ class AddBookmarkView: UIView {
         $0.font = .cellTitleFont
         $0.textColor = .label
         $0.isScrollEnabled = true
-        $0.keyboardType = .emailAddress
-        $0.autocorrectionType = .no
         $0.keyboardType = .URL
+        $0.autocorrectionType = .no
     }
     
     private let button = UIButton(type: .custom).then {
